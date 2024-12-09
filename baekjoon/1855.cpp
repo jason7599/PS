b127555afc7d0b138576f44934a0e0403bae5da3
jason7599 <jason7599@hanmail.net>
2024-12-09 11:23:21 +0900

    update

--- baekjoon/1855.cpp
+++ baekjoon/1855.cpp
@@ -3,12 +3,17 @@
 
 int main()
 {
-    int cols;
-    std::cin >> cols;
+    int n_cols;
+    std::cin >> n_cols;
 
     std::string pw;
     std::cin >> pw;
-
-    int rows = pw.length() / cols;
     
+    int n_rows = pw.length() / n_cols;
+
+    for (int i = 0; i < n_cols; i++)
+    {
+        for (int j = 0; j < n_rows; j++)
+            std::cout << pw[j * n_cols + (j % 2 ? n_cols - 1 - i : i)];
+    }
 }
