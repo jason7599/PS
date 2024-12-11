29c1a2e2f59ee32a19d2543728194d2c2163e9ec
Jason <jason7599@hanmail.net>
2024-12-12 08:46:01 +0900

    update

---
+++ baekjoon/5446_min_rm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_map>
+
+struct Node
+{
+    std::unordered_map<char, int> children;
+};
+
+Node root;
+
+int main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0), std::cout.tie(0);
+
+    int n_tc;
+    std::cin >> n_tc;
+
+    while (n_tc--)
+    {
+        
+    }
+}
