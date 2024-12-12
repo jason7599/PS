e7c5ef986746ce918e3391e2291ed65344c2141f
jason7599 <jason7599@hanmail.net>
2024-12-12 15:20:57 +0900

    update

--- baekjoon/5446_min_rm.cpp
+++ baekjoon/5446_min_rm.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
+#include <string>
 #include <unordered_map>
 
 struct Node
 {
-    std::unordered_map<char, int> children;
+    std::unordered_map<char, Node*> children;
+    bool end = 0;
+    bool do_rm = 0;
+    bool safe_to_rm = 1; // true if this node has no "dont-rm" subnodes
 };
 
-Node root;
+void add_file_name(Node* root, const std::string& file_name, bool do_rm)
+{
+    Node* node = root;
+
+    for (char c : file_name)
+    {
+        if (!do_rm)
+            node->safe_to_rm = 0;
+
+        if (!node->children.count(c))
+            node->children[c] = new Node(); // fuck
+
+        node = node->children[c];
+    }
+
+    node->end = 1;
+    node->safe_to_rm = do_rm;
+    node->do_rm = do_rm;
+}
+
+int get_min_rms(Node* node)
+{
+    if (node->safe_to_rm)
+        return 1;
+
+    int min_rms = 0;
+    if (node->do_rm && node->end) // without wildcard
+        min_rms++;
+
+    for (auto& [c, child] : node->children)
+        min_rms += get_min_rms(child);
+    
+    return min_rms;
+}
+
+void cleanup(Node* node)
+{
+    for (auto& [c, child] : node->children)
+    {
+        cleanup(child);
+        delete child;
+    }
+    node->children.clear();
+}
 
 int main()
 {
@@ -18,6 +65,22 @@ int main()
 
     while (n_tc--)
     {
-        
+        Node root;
+
+        for (int do_rm = 1; do_rm >= 0; do_rm--)
+        {
+            int n_files;
+            std::cin >> n_files;
+            while (n_files--)
+            {
+                std::string file_name;
+                std::cin >> file_name;
+                add_file_name(&root, file_name, do_rm);
+            }
+        }
+
+        std::cout << get_min_rms(&root) << '\n';
+
+        cleanup(&root);
     }
 }
