685b3f193e10a2e422d9ed23e9fe03815273837f
jason7599 <jason7599@hanmail.net>
2024-12-16 11:00:42 +0900

    update

--- baekjoon/2696_median.cpp
+++ baekjoon/2696_median.cpp
@@ -16,6 +16,8 @@ int main()
         std::priority_queue<int> lower; // lowerhalf, max pq
         std::priority_queue<int, std::vector<int>, std::greater<int>> upper; // min pq
 
+        std::cout << (n_elems + 1) / 2 << '\n';
+
         for (int i = 0; i < n_elems; i++)
         {
             int x;
@@ -26,6 +28,22 @@ int main()
             else
                 upper.push(x);
             
+            if (lower.size() > upper.size() + 1)
+            {
+                upper.push(lower.top());
+                lower.pop();
+            }
+            else if (upper.size() > lower.size())
+            {
+                lower.push(upper.top());
+                upper.pop();
+            }
+            
+            if (i % 2 == 0)
+                std::cout << lower.top() << ((i + 2) % 20 ? ' ' : '\n');
+
         }
+
+        std::cout << '\n';
     }
 }
