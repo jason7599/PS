bb8b9b81c6909cea027a8519ecbaca2d3db7fe39
jason7599 <jason7599@hanmail.net>
2024-12-17 11:48:00 +0900

    update

--- baekjoon/2371_file_distinction.cpp
+++ baekjoon/2371_file_distinction.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
 
 int main()
 {
@@ -8,5 +11,32 @@ int main()
     int n_files;
     std::cin >> n_files;
 
-    
+    std::vector<std::vector<int>> files(n_files);
+    size_t max_len = 0;
+
+    for (auto& file : files)
+    {
+        int x;
+        while (std::cin >> x && x != -1)
+            file.push_back(x);
+        
+        max_len = std::max(max_len, file.size());
+    }
+
+    std::vector<std::unordered_set<int>> uniques(max_len);
+
+    for (int i = 0; i < n_files; i++)
+    {
+        int j = 0;
+        for (; j < files[i].size(); j++)
+            uniques[j].insert(files[i][j]);
+        
+        for (; j < max_len; j++)
+            uniques[j].insert(-i - 1);
+    }
+
+    int k = 0;
+    for (; uniques[k].size() != n_files; k++);
+
+    std::cout << k + 1;
 }
