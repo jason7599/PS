391d441ef31422ad958733a6844d46110e9396f0
Jason <jason7599@hanmail.net>
2025-01-03 16:02:14 +0900

    rust support

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-g++ "$1" -o a.out && ./a.out && rm a.out
+#!/bin/bash
+
+if [[ "$1" == *.cpp ]]; then
+    g++ "$1" -o a.out && ./a.out && rm a.out
+elif [[ "$1" == *.rs ]]; then 
+    rustc "$1" -o a.out && ./a.out && rm a.out
+else
+    echo "wtf kind of file is this?"
+    exit 1
+fi
