ce533c1da6cf8d17c3b3a63dcbe28064f0baa7ac
jason7599 <jason7599@hanmail.net>
2025-01-16 09:56:07 +0900

    update

--- bash/9085_sum.sh
+++ bash/9085_sum.sh
@@ -1,7 +1,12 @@
-read n_tc
+#!/bin/bash
 
+read n_tc
 for ((i = 0; i < n_tc; i++)); do
-    read
-    read elems
-    echo "$elems" | awk '{ for (i = 1; i <= NF; i++) s += $i } END {print s}'
+    read n_elems
+    read -a arr
+    ((sum = 0))
+    for elem in "${arr[@]}"; do
+        ((sum += elem))
+    done
+    echo "$sum"
 done
