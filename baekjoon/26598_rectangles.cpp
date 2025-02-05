ee733038cce745e8dc7c76fa96b6143407077d8f
jason7599 <jason7599@hanmail.net>
2025-02-05 15:02:31 +0900

    update

---
+++ baekjoon/26598_rectangles.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0), std::cout.tie(0);
+
+    int height, width;
+    std::cin >> height >> width;
+
+    std::vector<std::string> board(height);
+
+    for (std::string& row : board)
+        std::cin >> row;
+
+    
+}
