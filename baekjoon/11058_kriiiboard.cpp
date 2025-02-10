24a9c506a4e504c96be6b4a52dd43955d8ab0bdd
jason7599 <jason7599@hanmail.net>
2025-02-10 13:35:10 +0900

    update

---
+++ baekjoon/11058_kriiiboard.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    int max_n_presses;
+    std::cin >> max_n_presses;
+
+    std::vector<int64_t> dp(max_n_presses + 1);
+
+    /**
+     * 1. Single A press:   1 press
+     * 2. Copy paste:       3 presses
+     * 3. print buffer:     1 presses 
+     */
+
+     
+}
