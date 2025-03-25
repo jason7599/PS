97785b5117f1abad1bcc79194b5422e601ca403f
jason7599 <jason7599@hanmail.net>
2025-03-25 09:59:14 +0900

    update

---
+++ baekjoon/20311.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n_tubes;
+int counts[300'000];
+int ans[300'000];
+
+bool solve(int idx) {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n_tubes;
+    for (int i = 0; i < n_tubes; i++) {
+        cin >> counts[i];
+    }
+}
