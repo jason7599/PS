355ad36ab71b094784c1e85de129fae4b206cc76
jason7599 <jason7599@hanmail.net>
2025-04-10 15:00:20 +0900

    update

---
+++ baekjoon/27311_latte_art.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int g_height, g_width;
+bool grid[10][10];
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n_tc;
+    cin >> n_tc;
+    while (n_tc--) {
+        cin >> g_height >> g_width;
+        
+    }
+}
