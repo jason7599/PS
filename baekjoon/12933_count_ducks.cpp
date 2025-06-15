153fe76fea6bd9d6ce3e9ece9fd9e1b5541f337a
jason7599 <jason7599@hanmail.net>
2025-06-15 15:52:12 +0900

    update

--- baekjoon/12933_count_ducks.cpp
+++ baekjoon/12933_count_ducks.cpp
@@ -18,30 +18,32 @@ int main() {
     string str;
     cin >> str;
 
-    int quack[5] = {0};
-    int n_quacks = 0; // ongoing
-    int n_ducks = 0; // total
+    int quack[4] = {};  // quack[i] = # of ducks who made it to the i-th character of quack 
+    int n_quacks = 0;   // # of unfinished quacks
+    int n_ducks = 0;    // # total ducks, both idle and in quack
 
     for (char c : str) {
         int t = qtoi(c);
-        quack[t]++;
         if (t == 0) { // 'q'
-            if (n_ducks <= n_quacks) { // there are no idle ducks
-                n_ducks++;
+            if (n_ducks == n_quacks) {  // there are no idle ducks
+                n_ducks++;              // new duck should be introduced
             }
-            n_quacks++;
+            n_quacks++;                 // quacking initiated
+            quack[0]++;
         } else {
-            if (!quack[t - 1]--) {
+            if (!quack[t - 1]--) {      // no current duck was expecting this character
                 goto bad;
             }
             if (t == 4) { // 'k'
-                n_quacks--;
+                n_quacks--;             // the quackination sequence has been finished 
+            } else {
+                quack[t]++;
             }
         }
     }
 
-    if (n_quacks) {
-bad:
+    if (n_quacks) { // unfinished quack remaining
+    bad:
         cout << -1 << '\n';
     } else {
         cout << n_ducks << '\n';
