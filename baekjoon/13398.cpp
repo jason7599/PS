d61903f00d7cab84015cd24e70fd422d0612a955
jason7599 <jason7599@hanmail.net>
2025-06-25 12:05:08 +0900

    update

---
+++ baekjoon/13398.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+
+int n, arr[100'000];
+int dp[100'000][2];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    
+}
