97b7db65b081316d1b6ef2d7e2537f92bbfb654c
jason7599 <jason7599@hanmail.net>
2025-06-27 13:29:38 +0900

    update

--- baekjoon/11781.cpp
+++ baekjoon/11781.cpp
@@ -4,7 +4,7 @@ using pii = pair<int, int>;
 
 struct Edge {
     int to;
-    int64_t time;
+    int64_t time;   // Time is already scaled by 2
     bool delay;
 };
 
@@ -13,8 +13,11 @@ vector<Edge> edges[5001];
 int64_t delay_start, delay_end;
 int64_t dmap[5001];
 
-int64_t solve() {
-    fill(&dmap[2], &dmap[1 + n_nodes], INT64_MAX);
+double solve() {
+    const int64_t INF = 1e18;
+    fill(&dmap[1], &dmap[1 + n_nodes], INF);
+    dmap[1] = 0;
+
     priority_queue<pair<int64_t, int>> pq;
     pq.push({0, 1});
 
@@ -32,29 +35,31 @@ int64_t solve() {
         max_dist = max(max_dist, cur_time);
 
         for (const auto& [nx_node, time, delay] : edges[cur_node]) {
-            // cout << cur_node << " -> " << nx_node << " at " << cur_time << ": "; 
-
             int64_t nx_time = cur_time;
-            if (delay && max(cur_time, delay_start) <= min(cur_time + time, delay_end)) { // delay happens during this movement
+
+            if (delay && max(cur_time, delay_start) <= min(cur_time + time, delay_end)) {
                 int64_t move_dist = 0;
-                if (delay_start > cur_time) { // delay starts during movement
-                    nx_time += delay_start - cur_time;
-                    move_dist += delay_start - cur_time;
+
+                if (delay_start > cur_time) {
+                    int64_t normal_time = delay_start - cur_time;
+                    nx_time += normal_time;
+                    move_dist += normal_time;
                 }
-                
-                // now the delay starts
+
+                // Delay starts
                 int64_t dist_left = time - move_dist;
-                int64_t delayed_movement_end = min(nx_time + 4 * dist_left, delay_end);
-                
-                move_dist += (delayed_movement_end - nx_time);
-                
-                nx_time = delayed_movement_end;
-                nx_time += time - move_dist;
+                int64_t delay_time_available = delay_end - nx_time;
+                int64_t delay_movement_time = min(2 * dist_left, delay_time_available);
+
+                int64_t delay_dist = delay_movement_time / 2;
+                move_dist += delay_dist;
+                nx_time += delay_movement_time;
+
+                // Finish remaining at normal speed
+                nx_time += (time - move_dist);
             } else {
                 nx_time += time;
             }
-
-            // cout << nx_time << '\n';
 
             if (dmap[nx_node] > nx_time) {
                 dmap[nx_node] = nx_time;
@@ -63,25 +68,24 @@ int64_t solve() {
         }
     }
 
-    return max_dist;
+    return max_dist / 2.0;
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-    
+
     int n_edges;
     cin >> n_nodes >> n_edges >> delay_start >> delay_end;
 
-    delay_start *= 4;
-    delay_end *= 4;
+    delay_start *= 2;
+    delay_end *= 2;
 
     while (n_edges--) {
         int from, to, time, to_delay, from_delay;
         cin >> from >> to >> time >> to_delay >> from_delay;
-
-        edges[from].push_back(Edge{to, time * 4, (bool)to_delay});
-        edges[to].push_back(Edge{from, time * 4, (bool)from_delay});
+        edges[from].push_back(Edge{to, time * 2, (bool)to_delay});
+        edges[to].push_back(Edge{from, time * 2, (bool)from_delay});
     }
 
-    cout << solve() / 4.0f << '\n';
+    cout << solve() << '\n';
 }
