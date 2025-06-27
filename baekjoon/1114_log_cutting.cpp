626c31320f50a03af3f88e52675a264caee4012c
jason7599 <jason7599@hanmail.net>
2025-06-27 10:20:52 +0900

    update

--- baekjoon/1114_log_cutting.cpp
+++ baekjoon/1114_log_cutting.cpp
@@ -2,64 +2,44 @@
 using namespace std;
 using pii = pair<int, int>;
 
-int total_len, n_spots, n_cuts;
-int spots[10'000];
-
-int f(int max_len) {
-    int prev = 0;
-    int t = n_cuts;
-    int r = -1;
-
-    for (int i = 0; i < n_spots; i++) {
-        if (spots[i] - prev >= max_len) {
-            if (!t--) {
-                return -1;
+pii f(const vector<int>& spots, int log_len, int max_len, int n_cuts) {
+    int last_cut = log_len;
+    for (int i = spots.size() - 1; i >= 0; i--) {
+        if (last_cut - spots[i] > max_len) {
+            if (i + 1 == spots.size() || spots[i + 1] - spots[i] > max_len || !n_cuts--) {
+                return {-1, 0};
             }
-            prev = spots[i];
-            if (r == -1) {
-                r = spots[i];
-            }
+            last_cut = spots[i + 1];
         }
     }
-
-    if (total_len - prev >= max_len) {
-        if (!t) {
-            return -1;
-        }
-    }
-
-    if (r == -1) {
-        r = spots[0];
-    }
-
-    cout << "f(" << max_len << ") = " << r << '\n'; 
-
-    return r;
+    return {last_cut, n_cuts};
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
-    cin >> total_len >> n_spots >> n_cuts;
-    for (int i = 0; i < n_spots; i++) {
+    int log_len, n_spots, n_cuts;
+    cin >> log_len >> n_spots >> n_cuts;
+
+    vector<int> spots(n_spots + 1);
+    for (int i = 1; i <= n_spots; i++) {
         cin >> spots[i];
     }
+    spots.front() = 0;
 
-    sort(spots, spots + n_spots);
+    sort(spots.begin(), spots.end());
 
-    int ans;
-    int ans2;
-    for (int l = 1, r = total_len; l <= r;) {
+    pii ans;
+    for (int l = 1, r = log_len - 1; l <= r;) {
         int m = (l + r) >> 1;
-        int t = f(m);
-        if (t != -1) {
-            ans = m;
-            ans2 = t;
+        auto [c, b] = f(spots, log_len, m, n_cuts);
+        if (c != -1) {
+            ans = {m, b ? spots[1] : c};
             r = m - 1;
         } else {
             l = m + 1;
         }
     }
 
-    cout << ans << ' ' << ans2 << '\n';
+    cout << ans.first << ' ' << ans.second << '\n';
 }
