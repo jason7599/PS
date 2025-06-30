624d5b7769c2a5708dd1ce7af1a6b2a0f503c48c
jason7599 <jason7599@hanmail.net>
2025-06-30 12:04:27 +0900

    update

--- baekjoon/27741_maze_game.cpp
+++ baekjoon/27741_maze_game.cpp
@@ -1,68 +1,60 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
 
-const int MAX_MAZE_SIZE = 300;
+int g_size;
+bool grid[300][300];
+char dp[300][300][301];
 
-int maze_size;
-bool maze_walls[MAX_MAZE_SIZE][MAX_MAZE_SIZE];
+bool is_oob(int y, int x) {
+    return y < 0 || y >= g_size || x < 0 || x >= g_size;
+}
 
-bool can_win(int y, int x, int max_move_dist)
-{
-    // 0: not cached, 1: win, -1: FUCKING lose BBBITCH
-    static char cache[MAX_MAZE_SIZE][MAX_MAZE_SIZE][MAX_MAZE_SIZE + 1];
+bool check(int y, int x) {
+    return !is_oob(y, x) && !grid[y][x];
+}
 
-    if (cache[y][x][max_move_dist])
-        return cache[y][x][max_move_dist] == 1;
+bool can_win(int y, int x, int k) {
+    if (!check(y + 1, x) && !check(y, x + 1)) {
+        return k != 1;
+    }
 
-    bool res = 0;
+    char& res = dp[y][x][k];
+    if (res) {
+        return res == 1;
+    }
 
-    for (int d = 0; d < 2; d++)
-    {
+    for (int d = 0; d < 2; d++) {
         int dy = d, dx = !d;
-
-        for (int dist = 1; dist <= max_move_dist; dist++)
-        {
-            if (max_move_dist % dist == 0)
-            {
-                if (dist != max_move_dist
-                && (dist != 1 || max_move_dist != 1))
-                {
-                    if (!can_win(y, x, dist))
-                    {
-                        res = 1;
-                        break;
-                    }
-                }
-            }
-
-            int ny = y + dy * dist;
-            int nx = x + dx * dist;
-
-            if (ny < maze_size && nx < maze_size
-            && !maze_walls[ny][nx])
-            {
-                if (!can_win(ny, nx, max_move_dist))
-                {
-                    res = 1;
-                    break;
-                }
+        for (int t = 1, ny = y + dy, nx = x + dx; t <= k && check(ny, nx); t++, ny += dy, nx += dx) {
+            if (!can_win(ny, nx, k)) {
+                return res = 1;
             }
         }
     }
 
-    cache[y][x][max_move_dist] = res ? 1 : -1;
-    return res;
-}
-
-int main()
-{
-    int max_move_dist;
-    std::cin >> maze_size >> max_move_dist;
-
-    for (int y = 0; y < maze_size; y++)
-    {
-        for (int x = 0; x < maze_size; x++)
-            std::cin >> maze_walls[y][x];
+    for (int nk = 1; nk <= k / 2; nk++) {
+        if (k % nk == 0) {
+            if (!can_win(y, x, nk)) {
+                return res = 1;
+            }
+        }
     }
 
-    std::cout << can_win(0, 0, max_move_dist);
+    res = -1;
+    return 0;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int k;
+    cin >> g_size >> k;
+    for (int y = 0; y < g_size; y++) {
+        for (int x = 0; x < g_size; x++) {
+            cin >> grid[y][x];
+        }
+    }
+
+    cout << can_win(0, 0, k) << '\n';
 }
