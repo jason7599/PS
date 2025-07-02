9cb62d0a3e546a9e1011285e2eaa7fc14a940cda
Jason <jason7599@hanmail.net>
2025-07-02 14:38:30 +0900

    update

--- baekjoon/1983.cpp
+++ baekjoon/1983.cpp
@@ -1,50 +1,51 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
 
-int board[2][401];
+int n;
+vector<int> arr1, arr2;
 
-/**
- * dp[c][i][j]: 열 c까지 고려했을때:
- *  첫번째 행에서 숫자 i개를 썼을 때
- *  두번째 행에서 숫자 j개를 썼을 때
- * 최댓값.
- */
-int dp[401][401][401];
- 
-int main()
-{
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0), std::cout.tie(0);
-
-    int n_cols, nz_cnt[2] = {0};
-    std::cin >> n_cols;
- 
-    for (int r = 0; r < 2; r++)
-    {
-        for (int c = 0; c < n_cols; c++)
-        {
-            int x; 
-            std::cin >> x;
-            if (x)
-                board[r][++nz_cnt[r]] = x;
-        }
+int f(int i1, int i2, int nz1, int nz2) {
+    if (i1 == arr1.size() || i2 == arr2.size()) {
+        return 0;
     }
+
+    int t = arr1[i1] * arr2[i2];
+    int res = t + f(i1 + 1, i2 + 1, nz1, nz2);
+    if (t > 0) {
+        return res;
+    }
+
+    if (nz1) {
+        res = max(res, f(i1, i2 + 1, nz1 - 1, nz2));
+    }
+
+    if (nz2) {
+        res = max(res, f(i1 + 1, i2, nz1, nz2 - 1));
+    }
+
+    return res;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
     
-    for (int c = 1; c <= n_cols; c++)
-    {
-        for (int i = 1; i <= std::min(nz_cnt[0], c); i++)
-        {
-            for (int j = 1; j <= std::min(nz_cnt[1], c); j++)
-            {
-                if (i == c && c == j) // 걍 다 붙여서 곱하는 거.
-                    dp[c][i][j] = 
-                        dp[c - 1][i - 1][j - 1] + board[0][i] * board[1][j];
-                else if (i == c)
-                    dp[c][i][j] = std::max(dp[c - 1][i - 1][j],
-                        dp[c - 1][i - 1][j - 1] + board[0][i] * board[1][j]
-                    );
-                
+    cin >> n;
+
+    int nz1, nz2;
+    for (int i = 0; i < 2; i++) {
+        vector<int>& arr = i ? arr1 : arr2;
+        int& nz = i ? nz1 : nz2;
+        for (int j = 0; j < n; j++) {
+            int x;
+            cin >> x;
+            if (x) {
+                arr.push_back(x);
+            } else {
+                nz++;
             }
         }
     }
+
+    cout << f(0, 0, nz1, nz2) << '\n';
 }
