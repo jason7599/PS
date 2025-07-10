c95ce9bc8c47b7e8a662a959281ba55ff9f95016
Jason <jason7599@hanmail.net>
2025-07-10 11:20:05 +0900

    update

--- baekjoon/1092_load.cpp
+++ baekjoon/1092_load.cpp
@@ -1,42 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 using pii = pair<int, int>;
+using ll = long long;
+
+int cranes[50];
+int boxes[10'000];
+int resps[50];
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-
-    int n_crane;
-    cin >> n_crane;
-    vector<int> cranes(n_crane);
-    for (int& i : cranes) {
-        cin >> i;
+    
+    int n_cranes;
+    cin >> n_cranes;
+    for (int i = 0; i < n_cranes; i++) {
+        cin >> cranes[i];
     }
+    sort(cranes, cranes + n_cranes);
 
-    int n_box;
-    cin >> n_box;
-    vector<int> boxes(n_box);
-    for (int& i : boxes) {
-        cin >> i;
+    int n_boxes;
+    cin >> n_boxes;
+    for (int i = 0; i < n_boxes; i++) {
+        cin >> boxes[i];
     }
+    sort(boxes, boxes + n_boxes);
 
-    sort(cranes.begin(), cranes.end());
-    sort(boxes.begin(), boxes.end());
-
-    if (cranes.back() < boxes.back()) {
+    if (cranes[n_cranes - 1] < boxes[n_boxes - 1]) {
         cout << -1 << '\n';
         return 0;
     }
 
-    vector<int> resps(n_crane);
-    for (int c = 0, b = 0; c < n_crane && b < n_box; c++) {
-        while (b < n_box && boxes[b] <= cranes[c]) {
-            resps[c]++, b++;
+    for (int c = 0, b = 0; c < n_cranes; c++) {
+        for (; b < n_boxes && cranes[c] <= boxes[b]; b++) {
+            resps[c]++;
         }
     }
 
-    for (int i = 0; i < n_crane; i++) {
-        cout << "resp of " << cranes[i] << ": " << resps[i] << '\n';
+    int time = 0;
+    while (1) {
+        bool done = 1;
+        for (int c = n_cranes - 1; c >= 0; c--) {
+            
+        }
     }
-
-    
+    cout << time << '\n';
 }
