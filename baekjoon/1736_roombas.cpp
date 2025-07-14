fd4b99e40ce52e883c53aad8405031295e29d0a1
Jason <jason7599@hanmail.net>
2025-07-14 10:07:51 +0900

    update

---
+++ baekjoon/1736_roombas.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define PRINT(x) {cout << x << '\n';}
+#define FOR(i, n) for(int i = 0; i < n; i++)
+#define FORR(i, s, e) for(int i = s; i <= e; i++)
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+}
