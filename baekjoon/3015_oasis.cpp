e9fec457195c22889693f50c7d3b3fe822087604
jason7599 <jason7599@hanmail.net>
2025-07-15 12:45:31 +0900

    update

--- baekjoon/3015_oasis.cpp
+++ baekjoon/3015_oasis.cpp
@@ -1,21 +1,40 @@
 #include <bits/stdc++.h>
+#define PRINT(x) {cout << x << '\n';}
+#define FOR(i, n) for(int i = 0; i < n; i++)
+#define FORR(i, s, e) for(int i = s; i <= e; i++)
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
 
-int main()
-{
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0), std::cout.tie(0);
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int n;
+    cin >> n;
 
-    int n_people;
-    std::cin >> n_people;
+    ll ans = 0;
+    stack<pii> stk; // <height, cnt>
 
-    std::stack<int> stk;
-    int64_t ans = 0;
-
-    while (n_people--)
-    {
+    while (n--) {
         int h;
-        std::cin >> h;
+        cin >> h;
 
-        
+        int t = 0;
+        while (!stk.empty() && stk.top().first <= h) {
+            ans += stk.top().second;
+            if (stk.top().first == h) {
+                t = stk.top().second;
+            }
+            stk.pop();
+        }
+
+        if (!stk.empty()) {
+            ans++;
+        }
+
+        stk.push({h, t + 1});
     }
+
+    PRINT(ans)
 }
