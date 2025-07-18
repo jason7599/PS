c995d7226be480fec959fa3c55fcd3409589a247
Jason <jason7599@hanmail.net>
2025-07-18 11:10:44 +0900

    update

--- baekjoon/2169_rover.cpp
+++ baekjoon/2169_rover.cpp
@@ -1,23 +1,48 @@
 #include <bits/stdc++.h>
-
+#define PRINT(x) {cout << x << '\n';}
+#define FOR(i, n) for(int i = 0; i < n; i++)
+#define FORR(i, s, e) for(int i = s; i <= e; i++)
 using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
 
-// l d r
-const int DIRS[3][2] = {{0, -1}, {1, 0}, {0, 1}};
+int g_h, g_w;
+int csum[1001][1001];
+int dp[1001][1001];
 
-int grid_h, grid_w;
-int grid[1000][1000];
-int dp[1000][1000][3];
+int get_sum(int y, int x0, int x1) {
+    return csum[y][x1] - csum[y][x0 - 1];
+}
+
+int f(int y, int x) {
+    if (y == g_h) {
+        return get_sum(g_h, x, g_w);
+    }
+
+    int& res = dp[y][x];
+    if (res != INT_MIN) {
+        return res;
+    }
+
+    FORR(nx, 1, g_w) {
+        res = max(res,
+            get_sum(y, min(x, nx), max(x, nx)) + f(y + 1, nx));
+    }
+    return res;
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
 
-    cin >> grid_h >> grid_w;
-    for (int y = 0; y < grid_h; y++) {
-        for (int x = 0; x < grid_w; x++) {
-            cin >> grid[y][x];
-            dp[y][x][0] = dp[y][x][1] = dp[y][x][2] = INT_MIN;
+    cin >> g_h >> g_w;
+    FORR(y, 1, g_h) {
+        FORR(x, 1, g_w) {
+            cin >> csum[y][x];
+            csum[y][x] += csum[y][x - 1];
+            dp[y][x] = INT_MIN;
         }
     }
+
+    PRINT(f(1, 1))
 }
