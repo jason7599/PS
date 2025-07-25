d9fc4d6b9e7813fd2a99fcc0f7f6dd9c1d2fa559
jason7599 <jason7599@hanmail.net>
2025-07-25 14:22:54 +0900

    update

---
+++ baekjoon/2133_tiling.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for(int i = s, _e = e; i <= _e; i++)
+#define REP(n) FOR(i, n)
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename T, typename... Args> void input(T& t, Args&... args) { cin >> t; input(args...); }
+template<typename T> void print(const T& t) { cout << t << '\n'; }
+template<typename T, typename... Args> void print(const T& t, const Args&... args) { cout << t << ' '; print(args...); }
+template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
+template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+}
