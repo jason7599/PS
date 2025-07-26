4d53806ad6b643c237ef0593e8a36a9ec0720c0c
Jason <jason7599@hanmail.net>
2025-07-26 14:16:41 +0900

    update

---
+++ baekjoon/13164_t_shirts.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for(int i = s, _e = e; i <= _e; i++)
+#define REP(n) FOR(i, n)
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename T, typename... Args> void input(T& t, Args&... args) { cin >> t; input(args...); }
+template<typename T> void print(const T& t) { cout << t << '\n'; }
+template<typename T, typename... Args> void print(const T& t, const Args&... args) { cout << t << ' '; print(args...); }
+template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
+template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n, arr[300'000];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int k;
+    input(n, k);
+    priority_queue<int> pq;
+    FOR(i, n) {
+        input(arr[i]);
+        if (i) {
+            pq.push(arr[i] - arr[i - 1]);
+        }
+    }
+
+    ll ans = arr[n - 1] - arr[0];
+    REP(k - 1) {
+        ans -= pq.top();
+        pq.pop();
+    }
+
+    print(ans);
+}
