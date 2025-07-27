96495065b81ee919c7dd02f823e4134032d3de7f
jason7599 <jason7599@hanmail.net>
2025-07-27 14:47:17 +0900

    update

---
+++ baekjoon/1182_subarr_sum.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename T, typename... Args> void input(T& t, Args&... args) { cin >> t; input(args...); }
+template<typename T> void print(const T& t) { cout << t << '\n'; }
+template<typename T, typename... Args> void print(const T& t, const Args&... args) { cout << t << ' '; print(args...); }
+template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
+template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n, arr[20];
+
+int f(int i, int sm, bool u) {    
+    int res = 0;
+    if (!sm && u) {
+        res++;
+    }
+
+    if (i == n) {
+        return res;
+    }
+
+    res += f(i + 1, sm, u);
+    res += f(i + 1, sm - arr[i], 1);
+
+    return res;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int sm;
+    input(n, sm);
+    FOR(i, n) input(arr[i]);
+    print(f(0, sm, 0));
+}
---
+++ baekjoon/1208_subarr_sum_2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename T, typename... Args> void input(T& t, Args&... args) { cin >> t; input(args...); }
+template<typename T> void print(const T& t) { cout << t << '\n'; }
+template<typename T, typename... Args> void print(const T& t, const Args&... args) { cout << t << ' '; print(args...); }
+template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
+template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n, arr[40];
+
+/**
+1s ~ 10^8 ops
+
+2^10 ~ 10^3
+2^40 ~ 10^12
+*/
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int target;
+    input(n, target);
+
+}
