c1999498b0088340e421f5a72cd527aced0003f0
jason7599 <jason7599@hanmail.net>
2025-07-28 14:22:07 +0900

    update

---
+++ baekjoon/1149_rgb_street.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename T, typename... Args> void input(T& t, Args&... args) { cin >> t; input(args...); }
+template<typename T> void print(const T& t) { cout << t << '\n'; }
+template<typename T, typename... Args> void print(const T& t, const Args&... args) { cout << t << ' '; print(args...); }
+template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
+template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n, arr[1000][3];
+int dp[1000][3];
+
+int f(int i, int p) {
+    if (i == n) {
+        return 0;
+    }
+
+    int& res = dp[i][p];
+    if (res) {
+        return res;
+    }
+
+    res = INT_MAX;
+    FOR(j, 3) {
+        if (j != p) {
+            upmin(res, arr[i][j] + f(i + 1, j));
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    FOR(i, input(n)) {
+        FOR(j, 3) {
+            input(arr[i][j]);
+        }
+    }
+
+    int ans = INT_MAX;
+    FOR(j, 3) {
+        upmin(ans, arr[0][j] + f(1, j));
+    }
+
+    print(ans);
+}
