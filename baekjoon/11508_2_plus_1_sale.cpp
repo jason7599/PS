117d5752504a59c0815e0622a9b5b813dbcea0a2
jason7599 <jason7599@hanmail.net>
2025-08-04 14:06:37 +0900

    update

---
+++ baekjoon/11508_2_plus_1_sale.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define LF {cout << '\n';}
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
+template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n, arr[100'000];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int sm = 0;
+    FOR(i, input(n)) {
+        sm += input(arr[i]);
+    }
+
+    sort(arr, arr + n);
+    for (int i = n - 3; i >= 0; i -= 3) {
+        sm -= arr[i];
+    }
+
+    print(sm);
+}
