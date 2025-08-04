636f9e0ce7a538790b75b46d8364eb67acbe2dc0
jason7599 <jason7599@hanmail.net>
2025-08-04 13:42:32 +0900

    update

--- baekjoon/2410.cpp
+++ baekjoon/2410.cpp
@@ -9,10 +9,10 @@ using namespace std;
 using pii = pair<int, int>;
 using ll = long long;
 using ull = unsigned long long;
-template<typename T = int> T input() { T x; cin >> x; return x; }
-template<typename T> T input(T& x) { cin >> x; return x; }
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
 template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
-template<typename... Args> tuple<Args...> inputs() { tuple<Args...> x; apply([](auto&... args){input(args...);}, x); return x; }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
 template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
 template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
 template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
@@ -22,32 +22,24 @@ const int MOD = 1e9;
 
 int dp[1'000'001][20];
 
-int f(int n, int b = 0) {
-    if (!n) {
-        return 1;
-    }
-
-    int bv = 1 << b;
-    if (bv > n) {
-        return -1;
-    }
-
-    int& res = dp[n][b];
-    if (res) {
-        return res;
-    }
-
-    res = f(n - bv, b);
-    int t = f(n - bv, b + 1);
-    if (t != -1) {
-        res = (res + t) % MOD;
-    }
-
-    return res;
-}
-
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
-    print(f(input()));
+    int n = input();
+
+    dp[0][0] = 1;
+    RANGE(i, 1, n) {
+        for (int p = 0; (1 << p) <= i; p++) {
+            FOR(j, p + 1) {
+                dp[i][p] = (dp[i][p] + dp[i - (1 << p)][j]) % MOD;
+            }
+        }
+    }
+
+    int ans = 0;
+    FOR(i, 20) {
+        ans = (ans + dp[n][i]) % MOD;
+    }
+
+    print(ans);
 }
