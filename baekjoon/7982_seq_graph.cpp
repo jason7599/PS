0cc9ca2fdf4949d9d63390c848a491f9ced62a0e
jason7599 <jason7599@hanmail.net>
2025-08-09 15:42:20 +0900

    update

---
+++ baekjoon/7982_seq_graph.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define LF {cout << '\n';}
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+const int MX = 1e6;
+
+int arr[MX];
+bool vis[MX + 2];
+
+int get_nx_min(int base) {
+    for (; vis[base]; base++);
+    return base;
+}
+
+vector<int> groups[MX];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int n = input();
+    FOR(i, n) {
+        input(arr[i]);
+    }
+
+    int nx_min = 1;
+    int n_groups = 0;
+    int i = 0;
+    for (; i < n;) {
+        for (; i < n; i++) {
+            vis[arr[i]] = 1;
+        }
+    }
+}
