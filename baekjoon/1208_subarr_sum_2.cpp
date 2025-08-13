08dd126ff3d7b2359d8921447d0976edb74cc7a4
jason7599 <jason7599@hanmail.net>
2025-08-13 22:53:04 +0900

    update

--- baekjoon/1208_subarr_sum_2.cpp
+++ baekjoon/1208_subarr_sum_2.cpp
@@ -2,6 +2,7 @@
 #define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
 #define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
 #define REP(n) FOR(__, n)
+#define LF {cout << '\n';}
 #define fi first
 #define se second
 using namespace std;
@@ -10,26 +11,82 @@ using ll = long long;
 using ull = unsigned long long;
 template<typename T = int> T input() { T t; cin >> t; return t; }
 template<typename T> T input(T& t) { cin >> t; return t; }
-template<typename T, typename... Args> void input(T& t, Args&... args) { cin >> t; input(args...); }
-template<typename T> void print(const T& t) { cout << t << '\n'; }
-template<typename T, typename... Args> void print(const T& t, const Args&... args) { cout << t << ' '; print(args...); }
-template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
-template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
 const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-int n, arr[40];
+int lens[2];
+int arrs[2][20];
 
-/**
-1s ~ 10^8 ops
+int comb_lens[2]{-1, -1};
+int combs[2][1 << 20];
 
-2^10 ~ 10^3
-2^40 ~ 10^12
-*/
+void bt_combs(int ai, int i, int s) {
+    if (i == lens[ai]) {
+        if (comb_lens[ai] == -1) {
+            comb_lens[ai] = 0;
+        } else {
+            combs[ai][comb_lens[ai]++] = s;
+        }
+        return;
+    }
+
+    bt_combs(ai, i + 1, s);
+    bt_combs(ai, i + 1, arrs[ai][i] + s);
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
-    int target;
-    input(n, target);
+    auto [n, s] = inputs<int, int>();
 
+    if (n == 1) {
+        print(input() == s);
+        return 0;
+    }
+
+    lens[0] = n / 2;
+    lens[1] = n - lens[0];
+
+    FOR(ai, 2) {
+        FOR(i, lens[ai]) {
+            input(arrs[ai][i]);
+        }
+    }
+
+    FOR(ai, 2) {
+        bt_combs(ai, 0, 0);
+        sort(combs[ai], combs[ai] + comb_lens[ai]);
+    }
+
+    ll ans = 0;
+    for (int l = 0, r = comb_lens[1] - 1; l < comb_lens[0] && r >= 0;) {
+        int sm = combs[0][l] + combs[1][r];
+        if (sm < s) {
+            l++;
+        } else if (sm > s) {
+            r--;
+        } else {
+            int l_cnt = 1, r_cnt = 1;
+            for (; l + l_cnt < comb_lens[0] && combs[0][l + l_cnt] == combs[0][l]; l_cnt++);
+            for (; r - r_cnt >= 0 && combs[1][r - r_cnt] == combs[1][r]; r_cnt++);
+
+            ans += (ll)l_cnt * r_cnt;
+            
+            l += l_cnt;
+            r -= r_cnt;
+        }
+    }
+
+    FOR(ai, 2) {
+        for (int t = lower_bound(combs[ai], combs[ai] + comb_lens[ai], s) - combs[ai];
+            t < comb_lens[ai] && combs[ai][t] == s; t++) {
+            ans++;
+        }
+    }
+
+    print(ans);
 }
