a187eff63c8ad3665b25b186c38c8bfefb712095
jason7599 <jason7599@hanmail.net>
2025-08-14 12:51:32 +0900

    update

--- baekjoon/30805_lexicographically_biggest_lcs.cpp
+++ baekjoon/30805_lexicographically_biggest_lcs.cpp
@@ -12,30 +12,47 @@ using ull = unsigned long long;
 template<typename T = int> T input() { T t; cin >> t; return t; }
 template<typename T> T input(T& t) { cin >> t; return t; }
 template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
-template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF}
-template<typename T> void upmax(T& v, const T& other) { v = max(v, other); }
-template<typename T> void upmin(T& v, const T& other) { v = min(v, other); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
 const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-int n, m;
-queue<int> idxs[2][101];
-
-int ans[100];
-int ans_len;
+int lens[2];
+queue<int> qs[2][101];
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
-    FOR(i, 2) {
-        FOR(j, input(i ? m : n)) {
-            idxs[i][input()].push(j);
+    FOR(ai, 2) {
+        FOR(i, input(lens[ai])) {
+            qs[ai][input()].push(i);
         }
     }
 
-    for (int i = 0, j = 0; i < n && j < m;) {
-        int ni = -1;
-        for (int v = 100; v > 0; v--) {
-            
+    int idxs[2]{};
+    vector<int> ans;
+    for (int mx = 100; mx > 0 && idxs[0] < lens[0] && idxs[1] < lens[1];) {
+        for (; mx > 0; mx--) {
+            FOR(ai, 2) {
+                for (; qs[ai][mx].size() && qs[ai][mx].front() < idxs[ai]; qs[ai][mx].pop());
+                if (qs[ai][mx].empty()) {
+                    goto cont_mx;
+                }
+            }
+
+            ans.push_back(mx);
+            FOR(ai, 2) {
+                idxs[ai] = qs[ai][mx].front() + 1;
+            }
+            break;
+
+            cont_mx:;
         }
     }
+
+    print(ans.size());
+    for (int i : ans) {
+        cout << i << ' ';
+    } LF
 }
