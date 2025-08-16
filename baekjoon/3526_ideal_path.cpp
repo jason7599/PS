47989c112ccd81e42b1a8b28604d615d94357c06
jason7599 <jason7599@hanmail.net>
2025-08-16 10:30:46 +0900

    update

---
+++ baekjoon/3526_ideal_path.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define LF {cout << '\n';}
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+const int MX_NODE = 100'001;
+
+int n_nodes;
+vector<pii> edges[MX_NODE]; // <to, color>
+vector<pii> prevs[MX_NODE];
+
+int vis[MX_NODE];
+bool opt_path[MX_NODE];
+
+void bfs() {
+    queue<int> q({1});
+    vis[1] = 1;
+
+    for (int q_size, time = 2; (q_size = q.size()); time++) {
+        while (q_size--) {
+            int cur = q.front();
+            q.pop();
+
+            for (const auto& [nxt, col] : edges[cur]) {
+                if (!vis[nxt] || vis[nxt] == time) {
+                    vis[nxt] = time;
+                    q.push(nxt);
+                    prevs[nxt].push_back({cur, col});
+                }
+            }
+        }
+    }
+}
+
+void mark_opt_path() {
+    queue<int> q({n_nodes});
+    opt_path[n_nodes] = 1;
+
+    while (q.size()) {
+        int cur = q.front();
+        q.pop();
+
+        for (const auto& [prv, _] : prevs[cur]) {
+            if (!opt_path[prv]) {
+                opt_path[prv] = 1;
+                q.push(prv);
+            }
+        }
+    }
+}
+
+vector<int> get_opt_path() {
+    vector<int> res;
+    vector<int> q({1});
+
+    while (1) {
+        vector<int> nxt_q;
+        int opt_col = INT_MAX;
+        for (int cur : q) {
+            for (const auto& [nxt, col] : edges[cur]) {
+                if (!opt_path[nxt] || vis[cur] >= vis[nxt]) {
+                    continue;
+                }
+                if (col < opt_col) {
+                    opt_col = col;
+                    nxt_q.clear();
+                    nxt_q.push_back(nxt);
+                } else if (col == opt_col) {
+                    nxt_q.push_back(nxt);
+                }
+            }
+        }
+        if (opt_col == INT_MAX) {
+            break;
+        }
+
+        res.push_back(opt_col);
+        q = nxt_q;
+    }
+
+    return res;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    input(n_nodes);
+    REP(input()) { // n_edges
+        auto [a, b, c] = inputs<int, int, int>();
+        edges[a].push_back({b, c});
+        edges[b].push_back({a, c});
+    }
+    
+    RANGE(i, 1, n_nodes) {
+        sort(edges[i].begin(), edges[i].end(), [](pii lhs, pii rhs) {
+            return lhs.se < rhs.se;
+        });
+    }
+
+    bfs();
+    mark_opt_path();
+
+    vector<int> path = get_opt_path();
+    print(path.size());
+    for (int i : path) {
+        cout << i << ' ';
+    } LF
+
+}
