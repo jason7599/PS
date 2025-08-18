02caeabf8ce0d4d8c123791c4d72fc763172120f
jason7599 <jason7599@hanmail.net>
2025-08-18 11:51:20 +0900

    update

--- baekjoon/12645.cpp
+++ baekjoon/12645.cpp
@@ -1,40 +1,32 @@
 #include <bits/stdc++.h>
-#define FOR(i, n) for(int i = 0; i < n; i++)
-#define RANGE(i, s, e) for(int i = s; i <= e; i++)
-#define REP(n) FOR(i, n)
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define LF {cout << '\n';}
+#define fi first
+#define se second
 using namespace std;
 using pii = pair<int, int>;
 using ll = long long;
 using ull = unsigned long long;
-template<typename T> T input() { T t; cin >> t; return t; }
-template<typename T> void input(T& t) { cin >> t; }
-template<typename T, typename... Args> void input(T& t, Args&... args) { cin >> t; input(args...); }
-template<typename T> void print(const T& t) { cout << t; }
-template<typename T, typename... Args> void print(const T& t, const Args&... args) { cout << t << ' '; print(args...); }
-template<typename T> void println(const T& t) { cout << t << '\n'; }
-template<typename T, typename... Args> void println(const T& t, const Args&... args) { print(t, args...); print('\n'); }
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
-    int n_tc;
-    input(n_tc);
-    RANGE(tc, 1, n_tc) {
-        string s;
-        input(s);
-        cout << "Case #" << tc << ": "; 
-        if (next_permutation(s.begin(), s.end())) {
-            println(s);
-        } else {
-            char m = '9';
-            bool z = 0;
-            for (char c : s) {
-                if (c == '0') {
-                    z = 1;
-                } else {
-                    m = min(m, c);
-                }
-            }
+    RANGE(tc, 1, input()) {
+        string s = input<string>();
+        cout << "Case #" << tc << ": ";
+        if (!next_permutation(s.begin(), s.end())) {
+            sort(s.begin(), s.end());
         }
+        print(s);
     }
 }
