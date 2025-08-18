8ed7a4cb748639fbaee6f977332ffeb0d0c1dff3
jason7599 <jason7599@hanmail.net>
2025-08-18 12:36:34 +0900

    update

---
+++ baekjoon/10942_palindrome_query.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define LF {cout << '\n';}
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n, arr[2001];
+char dp[2001][2001];
+
+bool f(int s, int e) {
+    if (s >= e) return 1;
+
+    char& res = dp[s][e];
+    if (res) return res == 1;
+
+    if (arr[s] == arr[e] && f(s + 1, e - 1)) {
+        res = 1;
+        return 1;
+    }
+
+    res = -1;
+    return 0;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    RANGE(i, 1, input(n)) input(arr[i]);
+    REP(input()) {
+        auto [s, e] = inputs<int, int>();
+        print(f(s, e));
+    }
+}
