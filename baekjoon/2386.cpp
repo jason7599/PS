819f0ad772bcfb11cd3276915fcba0f56b3df235
jason7599 <jason7599@hanmail.net>
2025-08-20 14:34:36 +0900

    update

---
+++ baekjoon/2386.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define LF {cout << '\n';}
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    for (char c; input(c) != '#';) {
+        string line;
+        getline(cin, line);
+        int cnt = 0;
+        for (char t : line) {
+            cnt += tolower(t) == tolower(c);
+        }
+        print(c, cnt);
+    }
+}
