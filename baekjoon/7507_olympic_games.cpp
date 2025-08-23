9421848ae771e47cf4c72b35c330bf22c004f8d6
jason7599 <jason7599@hanmail.net>
2025-08-23 10:18:14 +0900

    update

---
+++ baekjoon/7507_olympic_games.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i = 0, __n = n; i < __n; i++)
+#define RANGE(i, s, e) for(int i = s, __e = e; i <= __e; i++)
+#define REP(n) FOR(__, n)
+#define LF {cout << '\n';}
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n;
+pair<pii, int> arr[50000];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    RANGE(tc, 1, input()) {
+        FOR(i, input(n)) {
+            auto [d, s, e] = inputs<int, int, int>();
+            arr[i] = {{d, e}, s};
+        }
+        sort(arr, arr + n); // earliest ending first
+
+        int ans = 0;
+        pii last{};
+        FOR(i, n) {
+            if (last <= make_pair(arr[i].fi.fi, arr[i].se)) {
+                ans++;
+                last = arr[i].fi;
+            }
+        }
+
+        cout << "Scenario #" << tc << ":\n";
+        print(ans);
+        LF
+    }
+}
