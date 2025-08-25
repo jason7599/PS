a50945683e82cc1bf357ca0d31df97c5cf9a6cfe
jason7599 <jason7599@hanmail.net>
2025-08-25 13:30:53 +0900

    update

---
+++ baekjoon/21924_city_roads.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for (int i = s, _e = e; i <= _e; i++)
+#define REP(n) for (int _ = 0, _n = n; _ < _n; _++)
+#define STR(x) #x
+#define DBG(x) cerr << #x << " = " << x << '\n'
+#define EE(p) (ll)(1e##p)
+#define LF cout << '\n'
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF; }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n_nodes;
+int roots[EE(5) + 1];
+pair<int, pii> edges[5 * EE(5)];
+
+int find_root(int a) {
+    if (roots[a] != a) {
+        roots[a] = find_root(roots[a]);
+    }
+    return roots[a];
+}
+
+bool make_union(int a, int b) {
+    a = find_root(a);
+    b = find_root(b);
+    if (a == b) {
+        return 0;
+    }
+    roots[max(a, b)] = min(a, b);
+    return 1;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    n_nodes = input();
+    iota(roots + 1, roots + 1 + n_nodes, 1);
+
+    int n_edges = input();
+    ll total_cost = 0;
+    FOR(i, n_edges) {
+        auto [a, b, c] = inputs<int, int, int>();
+        edges[i] = {c, {a, b}};
+        total_cost += c;
+    }
+
+    sort(edges, edges + n_edges);
+
+    ll cost_sm = 0;
+    int union_cnt = 0;
+    FOR(i, n_edges) {
+        int c = edges[i].fi;
+        auto [a, b] = edges[i].se;
+
+        if (make_union(a, b)) {
+            cost_sm += c;
+            union_cnt++;
+        }
+    }
+
+    if (union_cnt < n_nodes - 1) {
+        print(-1);
+    } else {
+        print(total_cost - cost_sm);
+    }
+}
