f037aea21fe491624862143b3d84c0fa93727a13
jason7599 <jason7599@hanmail.net>
2025-08-28 13:52:14 +0900

    update

---
+++ baekjoon/2961.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for (int i = s, _e = e; i <= _e; i++)
+#define REP(n) for (int _ = 0, _n = n; _ < _n; _++)
+#define STR(x) #x
+#define DBG(x) cerr << #x << " = " << x << '\n'
+#define EE(p) (ll)(1e##p)
+#define LF cout << '\n'
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF; }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int n;
+pii arr[10];
+
+int f(int mask) {
+    int a = 1;
+    int b = 0;
+    FOR(t, n) {
+        if (mask & (1 << t)) {
+            a *= arr[t].fi;
+            b += arr[t].se;
+        }
+    }
+    return abs(a - b);
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    FOR(i, input(n)) {
+        input(arr[i].fi, arr[i].se);
+    }
+
+    int ans = INT_MAX;
+    RANGE(m, 1, (1 << n) - 1) {
+        upmin(ans, f(m));
+    }
+
+    print(ans);
+}
