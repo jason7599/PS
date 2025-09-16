2820de027da4e00973890f538f5363dcb0c5b7e1
Jason <jason7599@hanmail.net>
2025-09-16 12:32:57 +0900

    update

---
+++ baekjoon/32627.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (ll i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for (ll i = s, _e = e; i <= _e; i++)
+#define REP(n) for (ll _ = 0, _n = n; _ < _n; _++)
+#define STR(x) #x
+#define DBG(x) cerr << #x << " = " << x << '\n'
+#define EE(p) (ll)(1e##p)
+#define LF cout << '\n'
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename T, int C> array<T, C> inputs() { array<T, C> arr; for (T& t : arr) cin >> t; return arr; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF; }
+template<typename T> T upmax(T& v, const T& other) { v = max(v, other); return v; }
+template<typename T> T upmin(T& v, const T& other) { v = min(v, other); return v; }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+const int MAX_LEN = 3 * EE(5);
+
+pair<char, int> chars[MAX_LEN];
+bool erased[MAX_LEN];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    auto [len, n_ops, str] = inputs<int, int, string>();
+    FOR(i, len) {
+        chars[i] = {str[i], i};
+    }
+    sort(chars, chars + len);
+
+    FOR(i, n_ops) {
+        erased[chars[i].se] = 1;
+    }
+
+    FOR(i, len) {
+        if (!erased[i]) {
+            cout << str[i];
+        }
+    }
+    LF;
+}
