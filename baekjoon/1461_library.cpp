e60db07901a46780776f162d34f1f15388cc6057
jason7599 <jason7599@hanmail.net>
2025-09-27 12:45:50 +0900

    update

---
+++ baekjoon/1461_library.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for (int i = s, _e = e; i <= _e; i++)
+#define REP(n) for (int _ = 0, _n = n; _ < _n; _++)
+#define STR(x) #x
+#define DBG(x) cerr << #x << " = " << x << '\n'
+#define EE(p) (ll)(1e##p)
+#define LF cout << '\n'
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T& input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename T, int C> array<T, C> inputs() { array<T, C> arr; for (T& t : arr) cin >> t; return arr; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF; }
+template<typename T> T& upmax(T& v, const T& other) { return v = max(v, other); }
+template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
+
+int get_sm(vector<int>& v, int m) {
+    int sm = 0;
+    for (int i = v.size() - 1; i >= 0; i -= m) {
+        sm += v[i];
+    }
+    return sm;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    auto [n, m] = inputs<int, 2>();
+    vector<int> pos, neg;
+    REP(n) {
+        int x = input();
+        if (x > 0) {
+            pos.push_back(x);
+        } else {
+            neg.push_back(-x);
+        }
+    }
+
+    sort(pos.begin(), pos.end());
+    sort(neg.begin(), neg.end());
+
+    int ans = 2 * (get_sm(pos, m) + get_sm(neg, m)) - max(pos.empty() ? 0 : pos.back(), neg.empty() ? 0 : neg.back());
+    print(ans);
+}
