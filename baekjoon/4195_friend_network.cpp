b07216baeda19a755f1d28d80f8899a5aa715502
jason7599 <jason7599@hanmail.net>
2025-10-11 14:37:45 +0900

    update

--- baekjoon/4195_friend_network.cpp
+++ baekjoon/4195_friend_network.cpp
@@ -22,35 +22,58 @@ template<typename T> T& upmax(T& v, const T& other) { return v = max(v, other); 
 template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); }
 const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
 
-class DualPQ {
-private:
-    bool is_empty = 1;
-    int min_idx = 0;
-    int max_idx = 0;
-    int arr[1'000'001];
-public:
-    void insert(int n) {
-        if (is_empty) {
-            is_empty = 0;
-            min_idx = max_idx = 1;
-            arr[1] = n;
-            return;
-        }
+int edges[100'000][2];
+int roots[200'000];
+int sizes[200'000];
 
+int find_root(int i) {
+    if (roots[i] != i) {
+        roots[i] = find_root(roots[i]);
+    }
+    return roots[i];
+}
 
+void make_union(int a, int b) {
+    a = find_root(a);
+    b = find_root(b);
+
+    if (a == b) {
+        return;
     }
 
-    bool empty() const {
-        return is_empty;
-    }
-};
+    roots[max(a, b)] = min(a, b);
+    sizes[max(a, b)] += sizes[min(a, b)];
+    sizes[min(a, b)] = sizes[max(a, b)];
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-
+    
     REP(input()) {
-        REP(input()) {
+        int n_edges = input();
 
+        int n_nodes = 0;
+        unordered_map<string, int> ids;
+
+        FOR(i, n_edges) {
+            FOR(j, 2) {
+                auto s = input<string>();
+                if (ids.count(s) == 0) {
+                    ids[s] = n_nodes++;
+                }
+                edges[i][j] = ids[s];
+            }
+        }
+
+        FOR(i, n_nodes) {
+            roots[i] = i;
+            sizes[i] = 1;
+        }
+
+        FOR(i, n_edges) {
+            auto [a, b] = edges[i];
+            make_union(a, b);
+            print(sizes[find_root(a)]);
         }
     }
 }
