ccebad817797cacc24f2c594db2d966a9087469f
jason7599 <jason7599@hanmail.net>
2025-11-17 11:56:51 +0900

    update

--- baekjoon/23895.cpp
+++ baekjoon/23895.cpp
@@ -1,37 +1,48 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for (int i = s, _e = e; i <= _e; i++)
+#define REP(n) for (int _ = 0, _n = n; _ < _n; _++)
+#define STR(x) #x
+#define DBG(x) cerr << #x << " = " << x << '\n'
+#define EE(p) (ll)(1e##p)
+#define LF cout << '\n'
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T& input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename T, int C> array<T, C> inputs() { array<T, C> arr; for (T& t : arr) cin >> t; return arr; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF; }
+template<typename T> T& upmax(T& v, const T& other) { return v = max(v, other); }
+template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
 
-int main()
-{
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0), std::cout.tie(0);
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    RANGE(tc, 1, input()) {
+        auto [n, m] = inputs<int, int>();
 
-    int n_tc;
-    std::cin >> n_tc;
-
-    for (int tc = 1; tc <= n_tc; tc++)
-    {
-        int n_houses, budget;
-        std::cin >> n_houses >> budget;
-
-        std::priority_queue<int> pq;
-
-        while (n_houses--)
-        {
-            int price;
-            std::cin >> price;
-            pq.push(-price);
+        priority_queue<int> pq;
+        REP(n) {
+            int x = input();
+            if (x <= m) {
+                pq.push(-x);
+            }
         }
 
-        int n_houses_bought = 0;
-        while (!pq.empty() && -pq.top() <= budget)
-        {
-            budget += pq.top();
+        int res = 0;
+        while (pq.size() && m >= -pq.top()) {
+            res++;
+            m += pq.top();
             pq.pop();
-            n_houses_bought++;
         }
 
-        std::cout << "Case #" << tc << ": ";
-        std::cout << n_houses_bought << '\n';
+        cout << "Case #" << tc << ": " << res << '\n';
     }
 }
