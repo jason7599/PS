36fd2edca413cef8022a140f49485e7b60b5e5e9
jason7599 <jason7599@hanmail.net>
2025-12-15 14:18:58 +0900

    update

---
+++ baekjoon/25515_tree_node_path.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for (int i = s, _e = e; i <= _e; i++)
+#define REP(n) for (int _ = 0, _n = n; _ < _n; _++)
+#define STR(x) #x
+#define DBG(x) cerr << #x << " = " << x << '\n'
+#define EE(p) (ll)(1e##p)
+#define LF cout << '\n'
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T& input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename T, int C> array<T, C> inputs() { array<T, C> arr; for (T& t : arr) cin >> t; return arr; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF; }
+template<typename T> T& upmax(T& v, const T& other) { return v = max(v, other); }
+template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
+
+int node_vals[100'000];
+vector<int> edges[100'000];
+
+ll f(int node, int prev) {
+    ll res = node_vals[node];
+
+    for (int nxt : edges[node]) {
+        if (nxt != prev) {
+            ll t = f(nxt, node);
+            if (t > 0) {
+                res += t;
+            }
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    int n_nodes = input();
+    REP(n_nodes - 1) {
+        auto [a, b] = inputs<int, 2>();
+        edges[a].push_back(b);
+        edges[b].push_back(a);
+    }
+
+    FOR(i, n_nodes) {
+        input(node_vals[i]);
+    }
+
+    print(f(0, -1));
+}
