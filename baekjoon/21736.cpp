0e76b5f7f1a1362b7b07739d1c8fccab35f5d102
jason7599 <jason7599@hanmail.net>
2025-12-30 14:40:14 +0900

    update

--- baekjoon/21736.cpp
+++ baekjoon/21736.cpp
@@ -22,8 +22,28 @@ template<typename T> T& upmax(T& v, const T& other) { return v = max(v, other); 
 template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); }
 const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
 
+const char T_SRC = 'I';
+const char T_WALL = 'X';
+const char T_PERSON = 'P';
+const char T_EMPTY = 'O';
+
 int g_h, g_w;
 char grid[600][600];
+
+int dfs(int y, int x) {
+    int res = grid[y][x] == T_PERSON;
+    
+    grid[y][x] = T_WALL;
+    for (const auto& [dy, dx]: DIRS) {
+        int ny = y + dy;
+        int nx = x + dx;
+        if (0 <= ny && ny < g_h && 0 <= nx && nx < g_w && grid[ny][nx] != T_WALL) {
+            res += dfs(ny, nx);
+        }
+    }
+
+    return res;
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -32,30 +52,13 @@ int main() {
     int s_y, s_x;
     FOR(y, g_h) {
         FOR(x, g_w) {
-            if (input(grid[y][x]) == 'I') {
+            if (input(grid[y][x]) == T_SRC) {
                 s_y = y, s_x = x;
-                grid[y][x] = 'X';
             }
         }
     }
 
-    int ans = 0;
-    for (queue<pii> q({{s_y, s_x}}); q.size();) {
-        int y = q.front().fi;
-        int x = q.front().se;
-        q.pop();
-
-        for (const auto& [dy, dx] : DIRS) {
-            int ny = y + dy;
-            int nx = x + dx;
-
-            if (0 <= ny && ny < g_h && 0 <= nx && nx < g_w && grid[ny][nx] != 'X') {
-                ans += grid[ny][nx] == 'P';
-                grid[ny][nx] = 'X';
-                q.push({ny, nx});
-            }
-        }
-    }
+    int ans = dfs(s_y, s_x);
 
     if (ans) print(ans);
     else print("TT");
