c1040fe5bc0dee4b88f3525ef255b1f594dd72c6
jason7599 <jason7599@hanmail.net>
2026-01-09 21:30:29 +0900

    update

--- z_algorithm.cpp
+++ z_algorithm.cpp
@@ -21,26 +21,56 @@ template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); 
 const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
 
 vector<int> zfind(const string& text, const string& pattern) {
-    const string s = pattern + '$' + text;
+    const string s = pattern + '$' + text; // assuming both text and pattern does not contain '$'
 
     const int N = s.size();
     vector<int> z(N, 0);
     vector<int> res;
 
+    /*
+        i = the current position we are computing z[i] for.
+            we start from i = 1 because z[0] is useless
+        l and r describe the Z-box
+            [l..r] is a region that we already checked and know matches the prefix.
+    */
     for (int i = 1, l = 0, r = 0; i < N; i++) {
+        /*
+            case 1: we are inside the Z-box
+                i.e, we are standing inside a region we already verified
+                therefore we can reuse earlier computed information
+        */
         if (i <= r) {
+            /*
+                align with the prefix
+                    k is the matching position in the prefix.
+                z[k] = how much the prefix matched earlier
+                r - i + 1 = how much space we have left in the box.
+                we take min so that we don't protrude out of the box
+            */
             int k = i - l;
             z[i] = min(r - i + 1, z[k]);
         }
 
+        /*
+            try to extend the match
+            this is the ONLY place we compare characters
+        */
         while (i + z[i] < N && s[z[i]] == s[i + z[i]]) {
             z[i]++;
         }
 
+        /*
+            if z[i] == pattern.length(), this means a full match
+            i - pattern.length() + 1 to translate to starting index in text
+        */
         if (z[i] == pattern.length()) {
             res.push_back(i - pattern.length() + 1);
         }
 
+        /*
+            if the match at i reaches further right than before:
+                make this the new Z-box
+        */
         if (i + z[i] - 1 > r) { 
             l = i;
             r = i + z[i] - 1;
@@ -58,7 +88,7 @@ int main() {
     getline(cin, text);
     getline(cin, pattern);
 
-    auto z = f(text, pattern);
+    auto z = zfind(text, pattern);
     for (int i : z) {
         cout << i << ' ';
     }
