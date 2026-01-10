52774c005a6b02810a36f74a7ccf9ef10323881e
jason <jason7599@hanmail.net>
2026-01-10 09:50:48 +0900

    hi

---
+++ baekjoon/13277_big_number_mult.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for (int i = s, _e = e; i <= _e; i++)
+#define REP(n) for (int _ = 0, _n = n; _ < _n; _++)
+#define DBG(x) cerr << #x << " = " << x << '\n'
+#define LF cout << '\n'
+#define fi first
+#define se second
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+using ull = unsigned long long;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T& input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename T, int C> array<T, C> inputs() { array<T, C> arr; for (T& t : arr) cin >> t; return arr; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); LF; }
+template<typename T> T& upmax(T& v, const T& other) { return v = max(v, other); }
+template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
+
+class Number {
+private:
+    string str;
+public:
+    Number() : str("0") {};
+    Number(string s) : str(move(s)) {
+        if (!isdigit(s.front()) && s.front() != '-') {
+            throw 1;
+        }
+    }
+
+    friend istream& operator>>(istream& is, Number& n) {
+        is >> n.str;
+        return is;
+    }
+
+    friend ostream& operator<<(ostream& os, const Number& n) {
+        os << n.str;
+        return os;
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+ 
+    auto [a, b] = inputs<Number, 2>();
+}
---
+++ bignumber.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+class BigInt {
+private:
+    bool m_neg;
+    std::string m_str;
+
+public:
+    BigInt() : m_neg(false), m_str("0") {}
+    BigInt(std::string s) {
+        auto invalid = [&] { 
+            throw std::invalid_argument("Invalid BigInt string: \"" + s + '\"'); 
+        };
+
+        if (s.empty() || s == "-") invalid();
+
+        m_neg = s.front() == '-';
+
+        size_t t = s.find_first_not_of('0', m_neg);
+        if (t == std::string::npos) {
+            m_neg = false;
+            m_str = "0";
+            return;
+        }
+
+        for (size_t i = t; i < s.length(); ++i) {
+            if (!isdigit(static_cast<u_char>(s[i]))) {
+                invalid();
+            }
+        }
+
+        m_str = (m_neg ? "-" : "") + s.substr(t);
+    }
+
+    friend std::istream &operator>>(std::istream &is, BigInt &n) {
+        std::string s;
+        if (!(is >> s)) return is; // propagate EOF/fail
+
+        try {
+            BigInt tmp(std::move(s));
+            n = std::move(tmp);
+        } catch (const std::invalid_argument&) {
+            is.setstate(std::ios::failbit);
+        }
+        return is;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const BigInt &n) {
+        os << n.m_str;
+        return os;
+    }
+};
+
+int main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+
+    std::string s;
+    std::cin >> s;
+    
+    BigInt n(s);
+    std::cout << n << std::endl;
+}
