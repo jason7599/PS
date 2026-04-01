876061d0589baee253f1c845d7305f932b9cc8d1
jason7599 <jason7599@hanmail.net>
2026-04-01 15:39:27 +0900

    update

--- baekjoon/14567.cpp
+++ baekjoon/14567.cpp
@@ -15,7 +15,49 @@ template<typename T> T& upmax(T& v, const T& other) { return v = max(v, other); 
 template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); }
 const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
 
+vector<int> edges[1001];
+bool vis[1001];
+int res[1001];
+
+void toposort(int n, stack<int> &stk) {
+    vis[n] = true;
+    for (int nxt : edges[n]) {
+        if (!vis[nxt]) {
+            toposort(nxt, stk);
+        }
+    } 
+    stk.push(n);
+}
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
+    int n_nodes = input();
+    REP(input()) {
+        auto [a, b] = inputs<int, 2>();
+        edges[a].push_back(b);
+    }
+
+    stack<int> topo;
+    RANGE(n, 1, n_nodes) {
+        if (!vis[n]) {
+            toposort(n, topo);
+        }
+    }
+
+    for (; topo.size(); topo.pop()) {
+        int n = topo.top();
+        if (!res[n]) { // unmarked; meaning no immediate prereqs
+            res[n] = 1;
+        }
+
+        for (int nxt : edges[n]) {
+            upmax(res[nxt], res[n] + 1);
+        }
+    }
+
+    RANGE(n, 1, n_nodes) {
+        cout << res[n] << ' ';
+    }
+    print();
 }
