eed6e8494b8d96402a689d7020e5315f56dd9b12
jason7599 <jason7599@hanmail.net>
2026-04-06 12:33:53 +0900

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 if [[ "$1" == *.cpp ]]; then
     g++ "$1" -o a.out && ./a.out && rm a.out
 elif [[ "$1" == *.rs ]]; then 
     rustc "$1" -o a.out && ./a.out && rm a.out
 else
-    echo "wtf kind of file is this?"
+    echo "file not found?"
     exit 1
 fi
